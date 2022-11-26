f25b97091c86ace1dfe54fb3a1c03f21b2104ff3
akinaykut05 <akinaykut05@gmail.com>
2022-11-26 14:51:58 +0300

    Complated Day-13 exercises

---
+++ Day-13/main.py
@@ -0,0 +1,79 @@
+# Debugging
+
+# number = int(input("Which number do you want to check?"))
+#
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# Debug the code which one is upside
+
+# ---------------------------------------
+
+# number = int(input("Which number do you want to check?"))
+#
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+# ---------------------------------------
+
+# Debug the code which one is leap year program
+
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# --------------------------------------------------
+#
+# year = int(input("Which year do you want to check?"))
+#
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
+
+# -----------------------------------
+
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
+
+# ------------------------------------
+
+# for number in range(1, 101):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+#   elif number % 3 == 0:
+#     print("Fizz")
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
+
+
+
+
